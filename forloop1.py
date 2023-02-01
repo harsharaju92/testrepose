bf1f662734d368db9e9c765f2429cd4d9fc5b0ee
harsharaju92 <harsharaju92@gmail.com>
2023-02-01 12:26:37 +0530

    python  examples

---
+++ forloop1.py
@@ -0,0 +1,11 @@
+x='abcd'
+#to convert to upper case
+x = x.upper()
+print(x)
+y='PQRS'
+y=y.lower() # to convert to lower case
+print(y)
+#print(x)
+for i in range(len(x)):
+  #  i.upper()
+    print(i)
