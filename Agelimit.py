3ea1f5cbba96d0f6ec7b5f0aad290ec20d189a6f
harsharaju92 <harsharaju92@gmail.com>
2023-02-01 12:18:20 +0530

    python  examples

---
+++ Agelimit.py
@@ -0,0 +1,16 @@
+# cook your dish here
+
+MinAgelimit = 30
+MaxAgelimit = 40
+# print(MinAgelimit)
+# print(MaxAgelimit)
+Ageofchef = int(input("enter the age of the chef"))
+
+if (Ageofchef >= MinAgelimit and Ageofchef <= MaxAgelimit):
+
+    print("Chef is eligible to take the test")
+
+else :
+    print("chef is not eligible to take the test")
+
+
